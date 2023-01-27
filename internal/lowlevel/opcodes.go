12573b03883fb708f8fc3cce8a355c76da6fe7d3
Oscar Reparaz <oreparaz@users.noreply.github.com>
2023-01-26 22:18:33 -0800

    feat: add OP_MULTISIGVERIFY

--- internal/lowlevel/opcodes.go
+++ internal/lowlevel/opcodes.go
@@ -5,7 +5,7 @@ const OP_ADD = byte(1)
 const OP_MUL = byte(2)
 const OP_PUSH = byte(3)
 const OP_SIGVERIFY = byte(4)
-
+const OP_MULTISIGVERIFY = byte(5)
 const OP_AND = byte(6)
 const OP_OR  = byte(7)
 const OP_NOT = byte(8)
--- internal/machine/machine001_test.go
+++ internal/machine/machine001_test.go
@@ -24,3 +24,45 @@ func TestRunMachine001(t *testing.T) {
 
 	assert.True(t, RunMachine001(xPubKey, xSig, msg))
 }
+
+func helperTestMultisignature(msg, pk1, pk2, pk3, sig1, sig2 []byte) bool {
+	a := ll.Assembler{}
+	a.Append(ll.Push(sig1))
+	a.Append(ll.Push(sig2))
+
+	xSig := a.Code
+
+	b := ll.Assembler{}
+	b.Append(ll.Push(pk1))
+	b.Append(ll.Push(pk2))
+	b.Append(ll.Push(pk3))
+	b.Append(ll.Push1(2))
+	b.Append(ll.Push1(3))
+
+	b.Append(ll.MultisigVerify())
+
+	xPubKey := b.Code
+
+	return RunMachine001(xPubKey, xSig, msg)
+}
+
+func TestRunMachine2023012_Multisig(t *testing.T) {
+	msg := []byte("yolo")
+	_, pk1, sig1 := crypto.HelperVerifyData(msg)
+	_, pk2, sig2 := crypto.HelperVerifyData(msg)
+	_, pk3, sig3 := crypto.HelperVerifyData(msg)
+
+	assert.True(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig1, sig2))
+	assert.True(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig2, sig1))
+	assert.True(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig3, sig2))
+	assert.True(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig3, sig1))
+
+	// mind this case: repeated public keys are accepted and count towards quorum
+	assert.True(t, helperTestMultisignature(msg, pk1, pk1, pk3, sig1, sig2))
+
+	assert.False(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig1, sig1))
+	assert.False(t, helperTestMultisignature(msg, pk1, pk2, pk3, sig2, sig2))
+
+	assert.False(t, helperTestMultisignature(msg, pk1, pk1, pk3, sig2, sig3))
+	assert.False(t, helperTestMultisignature(msg, pk1, pk3, pk3, sig1, sig2))
+}
